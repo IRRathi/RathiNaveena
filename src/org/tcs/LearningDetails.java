ebb6a3cab100cf1f816f8b061d6b7584d2fb2fce
IRRathi <rathiir113@gmail.com>
2021-08-03 13:54:41 +0530

    java

---
+++ src/org/tcs/LearningDetails.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class LearningDetails {
+	public static void username() {
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		username();
+	}
+
+}
